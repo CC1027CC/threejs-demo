fdcc14784c18523fe0bdb2a8f04b41b21a3e2f12
陈城 <cc1027cc@163.com>
2022-01-27 22:32:58 +0800

    基本功能跑通

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using threejs_demo;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddThreejs();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
